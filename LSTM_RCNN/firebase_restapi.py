2d579962d267d8bf15e08646f18cd499ea12c505
Tarkus <michael.ali1995@gmail.com>
2017-10-28 06:16:32 -0400

    Rest API Changes

--- LSTM_RCNN/firebase_restapi.py
+++ LSTM_RCNN/firebase_restapi.py
@@ -1,15 +1,11 @@
+# Coomunicates with Firebase Rest API. Accepts file dump from lstm_weather.py and uploads to Firebase, where it is parsed and presented to user graphically.
 from firebase import firebase
 
 #Authentication
 def firebaseCall(file):
     firebase = firebase.FirebaseApplication('https://met-oracle.appspot.com/', authentication=None)
-
     authentication = firebase.Authentication('kriskringle123', 'testcasething@email.com', extra={'id': aAZxxqEfZqdxduBozhOPkVNdaev1})
     firebase.authentication = authentication
-    print(authentication.extra)
-
     user = authentication.get_user()
-    print(user.firebase_auth_tokem)
-
     Get = firebase.get(file, None)
     print(Get)
